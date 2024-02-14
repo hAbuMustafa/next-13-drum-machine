020607b3f766661379f396c815ec975e5d4412fa
Hosam Sultan <hosam5553@gmail.com>
2024-02-14 12:26:22 +0000

    implemented Provider

--- src/components/VolumeProvider/VolumeProvider.js
+++ src/components/VolumeProvider/VolumeProvider.js
@@ -1,7 +1,26 @@
-import React from 'react';
+'use client';
 
-function VolumeProvider() {
-  return <div></div>;
+import React, { createContext, useContext } from 'react';
+
+const VolumeContext = createContext();
+
+function VolumeProvider({ children }) {
+  const [volume, setVolume] = React.useState(true);
+  return (
+    <VolumeContext.Provider value={{ volume, setVolume }}>
+      {children}
+    </VolumeContext.Provider>
+  );
+}
+
+export function useVolume() {
+  const data = useContext(VolumeContext);
+
+  if (!data) {
+    throw new Error('useVolume must be used within a VolumeProvider');
+  }
+
+  return data;
 }
 
 export default VolumeProvider;
